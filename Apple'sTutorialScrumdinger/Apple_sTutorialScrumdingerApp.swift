3bcb6ef0fab4b7327394d29616f4df1d2527f0d2
KirTarasko <tarasko.kirill@icloud.com>
2023-06-07 09:51:16 +0200

    Initial Commit

---
+++ Apple'sTutorialScrumdinger/Apple_sTutorialScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_sTutorialScrumdingerApp.swift
+//  Apple'sTutorialScrumdinger
+//
+//  Created by Kirill Tarasko on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_sTutorialScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
