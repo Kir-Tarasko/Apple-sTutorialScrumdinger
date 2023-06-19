3248e48dfce7a629adf6eed4fb47387ed92989b0
KirTarasko <tarasko.kirill@icloud.com>
2023-06-19 22:41:16 +0200

    ProgressView

--- Apple'sTutorialScrumdinger/Apple_sTutorialScrumdingerApp.swift
+++ Apple'sTutorialScrumdinger/Apple_sTutorialScrumdingerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Apple_sTutorialScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }
